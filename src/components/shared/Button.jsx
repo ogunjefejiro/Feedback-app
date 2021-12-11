135073788cfc75e28681a11656de26da8bb29bcc
Fejiro Ogunje <ogunjefejiro@gmail.com>
2021-12-11 23:53:31 +0100

    new commit

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react"
+import FeedBackContext from "../../context/FeedBackContext"
+
 const Button = ({children, type, version, isDisabled}) => {
+
+    const {feedbackEdit} = useContext(FeedBackContext)
+
     return (
-        <button className={`btn btn-${version}`} type={type} disabled={isDisabled}>
+        <button className={`btn btn-${version} ${feedbackEdit.edit ? "edit-btn" : ""}`} type={type} disabled={isDisabled}>
             {children}
         </button>
     )
